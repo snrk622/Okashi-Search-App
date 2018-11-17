a6b73219eeea160c6e55c54acbc299ded7c1515c
snrk622 <s.0622.r@icloud.com>
2018-11-17 17:21:29 +0900

    更新

--- okashiSearchApp/ViewController.swift
+++ okashiSearchApp/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //search Barのdelegate通知先を設定
+        searchText.delegate = self
+        
+        //入力のヒントになる、プレースホルダを設定
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
 
-
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //検索ボタンをタップ時に実行
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //キーボードを閉じる
+        view.endEditing(true)
+        
+        if let searchWord = searchBar.text {
+            //デバックエリアに出力
+            print(searchWord)
+            //入力されていたら、お菓子を検索
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    
+    //searchOkashiメソッド
+    //第一引数：keyword 検索したいワード
+    func searchOkashi(keyword: String) {
+        //お菓子の検索キーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        //リクエストURLの組み立て
+        guard let req_url = URL(string: "http://www.sysbird.jp/toriko/api/?apikey=quest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+    }
 }
 
